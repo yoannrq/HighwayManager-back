fb6d8fa605f777794119f2750e81d69324f63c01
Yoann Rouquié <120712901+yoannrq@users.noreply.github.com>
2024-10-31 09:05:36 +0100

    fix: CORS configuration (Add PATCH method).

--- src/main/java/com/example/HighwayManager/config/SecurityConfig.java
+++ src/main/java/com/example/HighwayManager/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(frontendUrl));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
                 "Content-Type",
                 "Authorization",
